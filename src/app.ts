88b1ab40d21940c01c9874ef6d1067c1ed4595ca
DarkMerguez <jlaroche.69008@gmail.com>
2024-07-23 17:57:13 +0200

    début des routes produits

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,25 @@
-const massi : string = "Mas";
-console.log(massi);
+// Models :
+const { ProductCategory } = require("./database");
+const { EnterpriseCategory } = require("./database");
+const { Product } = require("./database");
+const { Carac } = require("./database");
+const { Image } = require("./database");
+const { User } = require("./database");
+const { Cart } = require("./database");
+const { Role } = require("./database");
+const { CaracProduct } = require("./database");
+const { ProductImage } = require("./database");
+const { ProductCart } = require("./database");
+const { Bill } = require("./database");
+const { BillProduct } = require("./database");
+const { Enterprise } = require("./database");
+const { Order } = require("./database");
+const { OrderProduct } = require("./database");
+
+
 
 const sequelize = require("./database");
+const { Op } = require("sequelize");
 
 
 const express = require("express");
@@ -16,7 +34,68 @@ app.use(cors());
 
 
 
+// ROUTES DE PRODUIT :
+
+app.get("/products", async (req, res) => {
+
+    try {
+        const products = await Product.findAll();
+
+        products.length > 0 ? res.status(200).json(products) : res.status(404).json({ message: "Aucun produit" });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Erreur lors de la recherche des Produits." });
+    }
+});
+
+
+
+app.get("/product/:id", async (req, res) => {
+    const product = await Product.findByPk(req.params.id)
+        .catch((error) => res.status(500).json("Erreur 500"));
+    if (product) {
+        res.status(200).json(product);
+    } else {
+        res.status(404).json({ message: "Aucun Produit trouvé avec cet id." });
+    }
+});
+
+
+
+app.get("/products/search/:text", async (req, res) => {
+    const text = req.params.text.toLowerCase();
+    try {
+        // doc sequelize : op like
+        const products = await Product.findAll({
+            where: {
+                [Op.or]: [
+                    {
+                        name: {
+                            [Op.like]: `%${text}%`
+                        }
+                    }
+                    ,
+                    {
+                        description: {
+                            [Op.like]: `%${text}%`
+                        }
+                    }
+                ]
+            }
+        });
+
+        products.length > 0 ? res.status(200).json(products) : res.status(404).json({ message: "Aucun Produit trouvé avec cette entrée." });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Erreur lors de la recherche des Produits." });
+    }
+});
+
+
+
 
 app.listen(8051, () => {
-    console.log("Youhouuuuu serveur lancé sur localhost:8066");
+    console.log("Youhouuuuu serveur lancé sur localhost:8051");
 });
